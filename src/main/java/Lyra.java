96c0120b745959b7ae7b19268035424b0488e15e
felix <puahtzefoong@gmail.com>
2026-01-23 10:19:14 +0800

    Update welcome message

--- src/main/java/Lyra.java
+++ src/main/java/Lyra.java
@@ -1,12 +1,15 @@
 public class Lyra {
     public static void main(String[] args) {
-        String welcomeString = "____________________________________________________________\n" + //
-                        " Hello! I'm Lyra\n" + //
-                        " What can I do for you?\n" + //
-                        "____________________________________________________________\n" + //
-                        " Bye. Hope to see you again soon!\n" + //
-                        "____________________________________________________________\n" + //
-                        "";
+        String welcomeString = 
+                        """
+                        ____________________________________________________________
+                         Hello! I'm Lyra
+                         What can I do for you?
+                        ____________________________________________________________
+                         Bye. Hope to see you again soon!
+                        ____________________________________________________________
+                        """ 
+        ;
         System.out.println(welcomeString);
     }
 }
