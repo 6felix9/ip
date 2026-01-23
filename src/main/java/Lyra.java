3b006c0b1470d6edda86d343ecee7de91a8afd7d
felix <puahtzefoong@gmail.com>
2026-01-23 10:51:03 +0800

    Add echo functionality and handle bye command

--- src/main/java/Lyra.java
+++ src/main/java/Lyra.java
@@ -1,18 +1,46 @@
+import java.util.Scanner;
+
 public class Lyra {
     public static void main(String[] args) {
-        String welcomeString = 
-                        """
-                        ____________________________________________________________
-                         Hello! I'm Lyra
-                         What can I do for you?
-                        ____________________________________________________________
-                        """;
+        Scanner scanner = new Scanner(System.in);
+
+        String welcomeString =
+                """
+                ____________________________________________________________
+                  Hello! I'm Lyra
+                  What can I do for you?
+                ____________________________________________________________
+                """;
+
         String exitString =
+                """
+                ____________________________________________________________
+                  Bye. Hope to see you again soon!
+                ____________________________________________________________
+                """;
+
+        System.out.println(welcomeString);
+
+        while (true) {
+            String echo = scanner.nextLine();
+            if (echo == null) break;
+
+            if (echo.equalsIgnoreCase("bye")) {
+                System.out.println(exitString);
+                break;
+            } else {
+                String echoString =
                         """
-                         Bye. Hope to see you again soon!
-                        ____________________________________________________________        
-                        """;
-        ;
-        System.out.println(welcomeString + exitString);
+                        ____________________________________________________________
+                          %s
+                        ____________________________________________________________
+                        """.formatted(echo);
+
+                System.out.println(echoString);
+            }
+        }
+
+        scanner.close();
+
     }
 }
