0b5df4d883d7b946456b0dc1fcd38a0ddf96dddd
felix <puahtzefoong@gmail.com>
2026-01-23 10:09:40 +0800

    Rename main class and add greeting

---
+++ src/main/java/Lyra.java
@@ -0,0 +1,12 @@
+public class Lyra {
+    public static void main(String[] args) {
+        String welcomeString = "____________________________________________________________\n" + //
+                        " Hello! I'm Lyra\n" + //
+                        " What can I do for you?\n" + //
+                        "____________________________________________________________\n" + //
+                        " Bye. Hope to see you again soon!\n" + //
+                        "____________________________________________________________\n" + //
+                        "";
+        System.out.println(welcomeString);
+    }
+}
