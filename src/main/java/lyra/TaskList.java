1fc264092a176da17d062997c1783a35f7afb070
felix <puahtzefoong@gmail.com>
2026-02-06 17:27:05 +0800

    Make tasks array final

--- src/main/java/lyra/TaskList.java
+++ src/main/java/lyra/TaskList.java
@@ -3,7 +3,7 @@ package lyra;
 import java.util.ArrayList;
 
 public class TaskList {
-    private ArrayList<Task> tasks;
+    private final ArrayList<Task> tasks;
 
     public TaskList(ArrayList<Task> tasks) {
         this.tasks = tasks;
